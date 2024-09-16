a926374693e0f73102c55abb918c6714cfa3e9a8
spengju <2658111307@qq.com>
2024-09-16 16:52:55 +0800

    初始化回调

--- extension/src/main/java/com/peng/demo/spring/beanDefinition/MyImportBeanDefinitionRegistry.java
+++ extension/src/main/java/com/peng/demo/spring/beanDefinition/MyImportBeanDefinitionRegistry.java
@@ -1,5 +1,6 @@
 package com.peng.demo.spring.beanDefinition;
 
+import com.peng.demo.spring.aware.event.User;
 import com.peng.demo.spring.service.PengService;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
@@ -23,11 +24,21 @@ public class MyImportBeanDefinitionRegistry implements ImportBeanDefinitionRegis
         BeanDefinition beanDefinition = builder.getBeanDefinition();
         beanDefinition.getPropertyValues().add("name","spengju");
         beanDefinition.getPropertyValues().add("age",25);
+
+//        beanDefinition.setLazyInit(true);
+//        beanDefinition.setScope();
+//        beanDefinition.setInitMethodName();
+
         /**
          * 可以获取当前@Import注解所在类的所有注解信息
          */
         Map<String, Object> annotationAttributes = importingClassMetadata.getAnnotationAttributes(ComponentScan.class.getName());
         System.out.println(annotationAttributes);
+
+        /**
+         * 动态设置构造函数
+         */
+//        beanDefinition.getConstructorArgumentValues().addIndexedArgumentValue(0, User.class);
 
         registry.registerBeanDefinition("pengService",beanDefinition);
     }

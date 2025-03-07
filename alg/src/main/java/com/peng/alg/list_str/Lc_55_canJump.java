778f4821bcf8d883289c85bd039eb99a4a87c058
spengju <2658111307@qq.com>
2025-03-07 14:21:41 +0800

    leetcode:55.跳跃游戏

---
+++ alg/src/main/java/com/peng/alg/list_str/Lc_55_canJump.java
@@ -0,0 +1,40 @@
+package com.peng.alg.list_str;
+
+/**
+ * @Author: spengju
+ * @Slogan: Day day no bug.
+ * @Date: 2025/3/7 10:48
+ * @Desc: 55. 跳跃游戏
+ * <p>
+ * 给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * <p>
+ * 判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,3,1,1,4]
+ * 输出：true
+ * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ */
+public class Lc_55_canJump {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; i++) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}

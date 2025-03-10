c3cd4d4c8f945151da023cb482d884c40602173a
spengju <2658111307@qq.com>
2025-03-10 10:17:10 +0800

    LeetCode: 45. 跳跃游戏 II

---
+++ alg/src/main/java/com/peng/alg/list_str/LC_45_jump.java
@@ -0,0 +1,71 @@
+package com.peng.alg.list_str;
+
+/**
+ * @Author: spengju
+ * @Slogan: Day day no bug.
+ * @Date: 2025/3/10 09:46
+ * @Desc: 45. 跳跃游戏 II
+ * 给定一个长度为 n 的 0 索引整数数组 nums。初始位置为 nums[0]。
+ * <p>
+ * 每个元素 nums[i] 表示从索引 i 向后跳转的最大长度。换句话说，如果你在 nums[i] 处，你可以跳转到任意 nums[i + j] 处:
+ * <p>
+ * 0 <= j <= nums[i]
+ * i + j < n
+ * 返回到达 nums[n - 1] 的最小跳跃次数。生成的测试用例可以到达 nums[n - 1]。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [2,3,1,1,4]
+ * 输出: 2
+ * 解释: 跳到最后一个位置的最小跳跃数是 2。
+ * 从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ * 示例 2:
+ * <p>
+ * 输入: nums = [2,3,0,1,4]
+ * 输出: 2
+ */
+public class LC_45_jump {
+
+    /**
+     * 方法一：反向查找出发位置
+     * 我们的目标是到达数组的最后一个位置，因此我们可以考虑最后一步跳跃前所在的位置，该位置通过跳跃能够到达最后一个位置。
+     * <p>
+     * 如果有多个位置通过跳跃都能够到达最后一个位置，那么我们应该如何进行选择呢？直观上来看，我们可以「贪心」地选择距离最后一个位置最远的那个位置，也就是对应下标最小的那个位置。因此，我们可以从左到右遍历数组，选择第一个满足要求的位置。
+     * <p>
+     * 找到最后一步跳跃前所在的位置之后，我们继续贪心地寻找倒数第二步跳跃前所在的位置，以此类推，直到找到数组的开始位置
+     *
+     * @param nums
+     * @return 有一天大象在吃冰淇淋，吃了好多好多，越吃越恶心，小石头看到了就说，象腻了，象腻了，你听见了吗，想你了～
+     */
+    public int jump(int[] nums) {
+        int position = nums.length - 1;
+        int steps = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (i + nums[i] >= position) {
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
+
+    }
+
+    public int jump2(int[] nums) {
+        int length = nums.length;
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+
+    }
+}
